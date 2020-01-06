a47846e8d9069b61a9c30636ac070db5c34bcc96
marachirascu <mara.chirascu@gmail.com>
2020-01-06 21:28:04 +0200

    Login frumos + Signup fara CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import store from './store';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import Login from './components/auth/Login.js';
-import SignupForm from './components//auth/Signup';
-import Dashboard from './components/Dashboard';
+import Login from "./components/auth/Login.js";
+import SignupForm from "./components//auth/Signup";
+import Dashboard from "./components/Dashboard";
 
-import './App.css';
-
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 // const express = require('express');
 // const bodyParser = require('body-parser');
@@ -17,24 +17,63 @@ import './App.css';
 // const app = express();
 
 function App() {
-	return (
-		<div className="App">
-			<Provider store={ store }>
-				<Router>
-					<div className="App">
-						<br/>
-						<Route exact path="/" component={ Login } />
-						<Route exact path="/signup" component={ SignupForm } />
-						<Route path="/dashboard" component={ Dashboard } />
-					</div>
-				</Router>
-			</Provider>
-		</div>
-  	);
-	// app.use(bodyParser.json());
-	// app.get('/', (req, res, next) => {
-	// 	res.send('Hello world!');
-	// })
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                            <div className="container">
+                                <Link className="navbar-brand" to={"/"}>
+                                    Home
+                                </Link>
+                                <div
+                                    className="collapse navbar-collapse"
+                                    id="navbarTogglerDemo02"
+                                >
+                                    <ul className="navbar-nav ml-auto">
+                                        <li className="nav-item">
+                                            <Link className="nav-link" to={"/"}>
+                                                Login
+                                            </Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link
+                                                className="nav-link"
+                                                to={"/signup"}
+                                            >
+                                                Sign up
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                        <div className="auth-wrapper">
+                            <div className="auth-inner">
+                                <Switch>
+                                    <Route exact path="/" component={Login} />
+                                    <Route
+                                        exact
+                                        path="/signup"
+                                        component={SignupForm}
+                                    />
+                                    <Route
+                                        path="/dashboard"
+                                        component={Dashboard}
+                                    />
+                                </Switch>
+                            </div>
+                        </div>
+                    </div>
+                </Router>
+            </Provider>
+        </div>
+    );
+    // app.use(bodyParser.json());
+    // app.get('/', (req, res, next) => {
+    // 	res.send('Hello world!');
+    // })
 }
 
 // app.listen(3000);
