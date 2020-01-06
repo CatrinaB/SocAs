0287ae658e4986621ba95aca64db7bcff5305baf
CatrinaB <catrina.bodean@yahoo.com>
2020-01-06 20:51:18 +0200

    Created Login.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import SignupForm from './components/Signup.js';
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Login from './components/auth/Login.js';
+import SignupForm from './components//auth/Signup';
+import Dashboard from './components/Dashboard';
+
 import './App.css';
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const graphqlHttp = require('express-graphql');
+
+// const app = express();
 
 function App() {
 	return (
 		<div className="App">
 			<Provider store={ store }>
-				<SignupForm />
+				<Router>
+					<div className="App">
+						<br/>
+						<Route exact path="/" component={ Login } />
+						<Route exact path="/signup" component={ SignupForm } />
+						<Route path="/dashboard" component={ Dashboard } />
+					</div>
+				</Router>
 			</Provider>
 		</div>
   	);
+	// app.use(bodyParser.json());
+	// app.get('/', (req, res, next) => {
+	// 	res.send('Hello world!');
+	// })
 }
+
+// app.listen(3000);
 
 export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+	render(){
+		return(
+			<div>
+				<p> Yey! </p>
+			</div>
+		);
+	}
+}
+
+export default Dashboard;
