f3502c8802a4c61ad30aba42fe9e43f2caf4b98c
marachirascu <mara.chirascu@gmail.com>
2020-01-07 18:19:11 +0200

    MBD nefericit

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -5,13 +5,30 @@ import {
     updatePassword,
     updateConfirmPassword
 } from "../../actions/authActions";
-import "../../styles/Signup.css";
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBBtn,
+    MDBInput,
+    MDBFormInline
+} from "mdbreact";
 
 class SignupForm extends React.Component {
+    state = {
+        radio: ""
+    };
+
     constructor(props) {
         super(props);
         this.onSubmit = this.onSubmit.bind(this);
     }
+
+    onClick = nr => () => {
+        this.setState({
+            radio: nr
+        });
+    };
 
     onChange(e) {
         switch (e.target.name) {
@@ -37,63 +54,104 @@ class SignupForm extends React.Component {
     render() {
         return (
             <div>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Name</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter first name"
-                            name="name"
-                            onChange={this.onChange}
-                            value={this.props.user}
-                        />
-                    </div>
-
-					<div className="form-group">
-                        <label>Email</label>
-                        <input
-                            type="email"
-                            className="form-control"
-                            placeholder="Enter email"
-                            name="email"
-                            onChange={this.onChange}
-                            value={this.props.user}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            placeholder="Enter password"
-                            name="password"
-                            onChange={this.onChange}
-                            value={this.props.password}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Confirm password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            placeholder="Enter password"
-                            name="confirmPassword"
-                            onChange={this.onChange}
-                            value={this.props.confirmPassword}
-                        />
-                    </div>
-
-					<button type="submit" className="btn btn-primary btn-block">
-						Create my account!
-                    </button>
-
-    				<p className="no-account-yet text-right">
-                        <a href="#">No account yet?</a>
-                    </p>
-				</form>
+                <form>
+                    <MDBContainer>
+                        <MDBRow className="d-flex mt-3 justify-content-center">
+                            <MDBCol md="6">
+                                <form>
+                                    <p className="h5 text-center mb-4">
+                                        Sign up
+                                    </p>
+                                    <div className="mt-5 grey-text">
+                                        <MDBInput
+                                            label="Name"
+                                            // icon="user"
+                                            group
+                                            type="text"
+                                            validate
+                                            error="wrong"
+                                            success="right"
+                                            placeholder=""
+                                            name="name"
+                                            onChange={this.onChange}
+                                            value={this.props.user}
+                                        />
+                                        <MDBInput
+                                            label="Email"
+                                            // icon="envelope"
+                                            group
+                                            type="email"
+                                            validate
+                                            error="wrong"
+                                            success="right"
+                                            placeholder=""
+                                            name="email"
+                                            onChange={this.onChange}
+                                            value={this.props.user}
+                                        />
+                                        <MDBInput
+                                            label="Password"
+                                            // icon="lock"
+                                            group
+                                            type="password"
+                                            validate
+                                            placeholder=""
+                                            name="password"
+                                            onChange={this.onChange}
+                                            value={this.props.password}
+                                        />
+                                        <MDBInput
+                                            label="Confirm password"
+                                            // icon="exclamation-triangle"
+                                            group
+                                            type="password"
+                                            validate
+                                            error="wrong"
+                                            success="right"
+                                            placeholder=""
+                                            name="confirmPassword"
+                                            onChange={this.onChange}
+                                            value={this.props.confirmPassword}
+                                        />
+                                    </div>
+                                    <MDBFormInline className="d-flex justify-content-center">
+                                        <MDBInput
+                                            gap
+                                            onClick={this.onClick(1)}
+                                            checked={
+                                                this.state.radio === 1
+                                                    ? true
+                                                    : false
+                                            }
+                                            label="Assistant"
+                                            type="radio"
+                                            id="radio1"
+                                            containerClass="mr-5"
+                                        />
+                                        <MDBInput
+                                            gap
+                                            onClick={this.onClick(2)}
+                                            checked={
+                                                this.state.radio === 2
+                                                    ? true
+                                                    : false
+                                            }
+                                            label="Person with disability"
+                                            type="radio"
+                                            id="radio2"
+                                            containerClass="mr-5"
+                                        />
+                                    </MDBFormInline>
+                                    <div className="text-center d-flex justify-content-center">
+                                        <MDBBtn color="primary" >
+                                            Register
+                                        </MDBBtn>
+                                    </div>
+                                </form>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
+                </form>
             </div>
         );
     }
