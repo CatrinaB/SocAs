7687fc3343ed7822434373df5dc3eba147d36c8f
Burbon13 <razvanroatis91@gmail.com>
2020-05-16 15:46:13 +0300

    Some margin/arrangements done

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -12,6 +12,9 @@ import { MuiPickersUtilsProvider, KeyboardDatePicker, } from '@material-ui/picke
 import DateFnsUtils from "@date-io/date-fns";
 
 
+const FORM_ITEMS_MARGIN = "10px"
+
+
 class Account extends React.Component {
 	constructor(props) {
 		super(props);
@@ -19,18 +22,23 @@ class Account extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<div>
+			<div style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}>
+				<div style={{ width: "40%" }}>
 					<form style={{
 						display: "flex",
 						flexDirection: "column",
 						justifyContent: "center",
-						alignItems: "center"
+						spaceBetween: "20px"
 					}}>
-						<Avatar alt="Sexy profile picture" src="http://localhost:3000/avatar.jpg"/>
-						<TextField id="email" label="E-mail"/>
-						<TextField id="name" label="Name"/>
-						<FormControl>
+						<Avatar alt="Sexy profile picture" src="http://localhost:3000/avatar.png"
+								style={{ margin: FORM_ITEMS_MARGIN }}/>
+						<TextField id="email" label="E-mail" style={{ margin: FORM_ITEMS_MARGIN }}/>
+						<TextField id="name" label="Name" style={{ margin: FORM_ITEMS_MARGIN }}/>
+						<FormControl style={{ margin: FORM_ITEMS_MARGIN }}>
 							<InputLabel>Gender</InputLabel>
 							<Select id="gender">
 								<MenuItem>Man</MenuItem>
@@ -39,12 +47,14 @@ class Account extends React.Component {
 							<FormHelperText>Helper text</FormHelperText>
 						</FormControl>
 						<MuiPickersUtilsProvider utils={DateFnsUtils}>
-							<KeyboardDatePicker>
+							<KeyboardDatePicker style={{ margin: FORM_ITEMS_MARGIN }}>
 							</KeyboardDatePicker>
 						</MuiPickersUtilsProvider>
-						<Button variant="contained" color="secondary" startIcon={<EmojiPeopleIcon/>}>Update
+						<Button variant="contained" color="secondary" style={{ margin: FORM_ITEMS_MARGIN }}
+								startIcon={<EmojiPeopleIcon/>}>Update
 							details</Button>
-						<Button variant="contained" color="secondary" startIcon={<DeleteIcon/>}>Delete account</Button>
+						<Button variant="contained" color="secondary" style={{ margin: FORM_ITEMS_MARGIN }}
+								startIcon={<DeleteIcon/>}>Delete account</Button>
 					</form>
 				</div>
 			</div>
